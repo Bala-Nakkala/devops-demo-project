b767c762e7c01531f43a4f20450adbf418b92272
Balu@N <balakrishna.nakkala@gmail.com>
2026-06-08 05:56:06 +0000

    list_pods

---
+++ python-k8s/list_pods.py
@@ -0,0 +1,10 @@
+from kubernetes import client, config
+
+config.load_kube_config()
+
+v1 = client.CoreV1Api()
+
+pods = v1.list_pod_for_all_namespaces()
+
+for pod in pods.items:
+    print(f"{pod.metadata.namespace} | {pod.metadata.name} | {pod.status.phase}")
